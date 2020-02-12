fef650f895f6370951261e5fbcb37a51fde99d2b
Jonathan Lemus <lemusj27@hotmail.com>
2020-02-12 16:58:16 -0500

    Added a test to the test class that will not pass. Fails if you go to build using gradlew build.

--- src/test/java/com/vaj/shoppingcart/AppTest.java
+++ src/test/java/com/vaj/shoppingcart/AppTest.java
@@ -11,4 +11,9 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+    
+    @Test public void testAppHasNoGreeting() {
+        App classUnderTest = new App();
+        assertNull("app shouldn't have a greeting", classUnderTest.getGreeting());
+    }
 }
