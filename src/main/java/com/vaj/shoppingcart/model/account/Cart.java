ab87abcb7d6d5db82dfca964c7cc3f0baf28dc36
Jonathan Lemus <lemusj27@hotmail.com>
2020-02-19 14:49:40 -0500

    Added basic methods to Cart

--- src/main/java/com/vaj/shoppingcart/model/account/Cart.java
+++ src/main/java/com/vaj/shoppingcart/model/account/Cart.java
@@ -1,4 +1,56 @@
 package com.vaj.shoppingcart.model.account;
 
+import java.util.List;
+
+import com.vaj.shoppingcart.model.product.Product;
+
 public class Cart {
+	
+	private List<Product> currentItemsInCart;
+	private double currentCartSubTotal;
+	private double currentTax;
+	private double currentTotal;
+	
+	public Cart() {
+		
+	}
+	
+	public void switchToProcessOrder() {
+		//Need to write the switch from screens or whatever we're doing with this
+	}
+	
+	public void clearCart() {
+		currentItemsInCart.clear();
+	}
+	
+	public void addItemToCart(Product product) {
+		product.getPrice();
+		currentCartSubTotal += product.getPrice();
+		currentTax += (product.getPrice() * 0.08625);
+		currentTotal = (currentCartSubTotal + currentTax);
+		currentItemsInCart.add(product);
+	}
+	
+	public void removeItemFromCart(Product product) {
+		currentCartSubTotal -= product.getPrice();
+		currentTax -= (product.getPrice() * 0.08625);
+		currentTotal = (currentCartSubTotal + currentTax);
+		currentItemsInCart.remove(product);
+	}
+	
+	public double getCurrentCartSubTotal() {
+		return currentCartSubTotal;
+	}
+	
+	public double getCurrentTax() {
+		return currentTax;
+	}
+	
+	public double getCurrentTotal() {
+		return currentTotal;
+	}
+	
+	public List<Product> getCurrentItemsInCart() {
+		return currentItemsInCart;
+	}
 }
