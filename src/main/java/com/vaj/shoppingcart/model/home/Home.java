164be56911aab7726e6d7b2ec2b2d3c3cbdb1d84
Jonathan Lemus <lemusj27@hotmail.com>
2020-02-20 23:43:13 -0500

    Barebones Home class Might not be necessary in this aspect

---
+++ src/main/java/com/vaj/shoppingcart/model/home/Home.java
@@ -0,0 +1,28 @@
+package com.vaj.shoppingcart.model.home;
+
+import com.vaj.shoppingcart.model.account.User;
+
+public class Home {
+	
+	private User currentUserLoggedIn;
+	
+	public Home() {
+		
+	}
+	
+	public void switchToSearch() {
+		
+	}
+	
+	public void switchToCart() {
+		
+	}
+	
+	public void switchToAccountInformation() {
+		
+	}
+	
+	public void accessWarehouse() {
+		
+	}
+}
